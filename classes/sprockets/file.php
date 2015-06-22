b36cd9ad8f4b8103924b709e313f3b434946df06
megri <martin@baramartin.se>
2015-06-23 00:38:23 +0200

    create directories recursively

--- classes/sprockets/file.php
+++ classes/sprockets/file.php
@@ -54,7 +54,12 @@ class Sprockets_File
 	 */
 	public function save_file($file_path, $source)
 	{
-		$path = trim($file_path);
+                $path = trim($file_path);
+                $dir = dirname($path);
+                if (!file_exists($dir)) {
+                        mkdir($dir, 0777, true);
+                }
+
 		$successful = (file_put_contents($path, $source) !== false);
 
 		if ( !$successful ) {
